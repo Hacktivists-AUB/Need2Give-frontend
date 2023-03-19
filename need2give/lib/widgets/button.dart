28413546625cd188312dd0a60eab71982e41d735
day <lilitmousheghyan@gmail.com>
2023-03-19 17:02:09 +0200

    Add custom button widget

---
+++ need2give/lib/widgets/button.dart
@@ -0,0 +1,31 @@
+import 'package:need2give/constants/global.dart';
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const Button({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Global.green,
+        minimumSize: const Size(double.infinity, 50),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(fontSize: 18),
+      ),
+    );
+  }
+}
