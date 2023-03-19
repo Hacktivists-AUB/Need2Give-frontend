f9c5a4aa6c0fc69c419442b5ee9b49c8b8f99844
day <lilitmousheghyan@gmail.com>
2023-03-19 16:57:20 +0200

    Add basic colors of the color scheme

---
+++ need2give/lib/constants/global.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+class Global {
+  static const darkGreen =Color.fromARGB(255, 57, 122, 76);
+  static const mediumGreen = Color.fromARGB(255, 86, 147, 88);
+  static const green = Color.fromARGB(255, 119, 192, 99);
+  static const lightGreen = Color.fromARGB(255, 190, 216, 146);
+  static const white = Colors.white;
+  static const lightGrey = Color.fromARGB(255, 224, 227, 214);
+  static const darkGrey = Color.fromARGB(255, 32, 32, 31);
+}
